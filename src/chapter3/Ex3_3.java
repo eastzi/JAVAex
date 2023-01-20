dabc1ceef49c11a0476d5f23ca2581c0e260e1f6
eastzi <zero7038@naver.com>
2023-01-21 00:29:41 +0900

    chapter 2, 3 연습문제(변수, 연산자)

---
+++ src/chapter3/Ex3_3.java
@@ -0,0 +1,15 @@
+package chapter3;
+
+public class Ex3_3 {
+
+	public static void main(String[] args) {
+		int num = 10; 
+		int num2 = -1;
+		int num3 = 0; 
+		
+		System.out.println(num > 0 ? "양수" : (num < 0 ? "음수" : "0"));
+		System.out.println(num2 > 0 ? "양수" : (num2 < 0 ? "음수" : "0"));
+		System.out.println(num3 > 0 ? "양수" : (num3 < 0 ? "음수" : "0"));
+	}
+
+}
