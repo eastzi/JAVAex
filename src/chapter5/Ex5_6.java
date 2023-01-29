6658adf24d4c98cb356d2340f53558653e180db7
eastzi <zero7038@naver.com>
2023-01-29 22:55:07 +0900

    chapter 5(배열)

--- src/chapter5/Ex5_6.java
+++ src/chapter5/Ex5_6.java
@@ -5,7 +5,7 @@ public class Ex5_6 {
 	public static void main(String[] args) {
 		int[] coinUnit = {500, 100, 50, 10};
 		
-		int money = 3400;
+		int money = 3170;
 		System.out.println("money = " + money);
 		
 		for(int i = 0; i < coinUnit.length; i++) {
